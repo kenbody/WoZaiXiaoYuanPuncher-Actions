3bd6ee8672771a783e9d7ce50055ea97eef54d02
WhiteFan <46106423+baifan97@users.noreply.github.com>
2021-11-14 15:12:18 +0800

    Update wzxy-healthcheck.py

--- wzxy-healthcheck.py
+++ wzxy-healthcheck.py
@@ -174,7 +174,7 @@ class WoZaiXiaoYuanPuncher:
             print("消息经pushplus推送成功")
         if os.environ.get('GOBOT_URL'):
             # go_cqhttp 推送
-            url = f'{push_config.get("GOBOT_URL")}?access_token={push_config.get("GOBOT_TOKEN")}&{push_config.get("GOBOT_QQ")}&message=⏰ 我在校园打卡结果通知\n---------\n\n打卡项目：健康打卡\n\n打卡情况：{notifyResult}\n\n打卡时间: {notifyTime}'
+            url = f'{os.environ.["GOBOT_URL"]}?access_token={push_config.get("GOBOT_TOKEN")}&{push_config.get("GOBOT_QQ")}&message=⏰ 我在校园打卡结果通知\n---------\n\n打卡项目：健康打卡\n\n打卡情况：{notifyResult}\n\n打卡时间: {notifyTime}'
             response = requests.get(url).json()
             if response["status"] == "ok":
                 print("消息经go-cqhttp 推送成功！")
